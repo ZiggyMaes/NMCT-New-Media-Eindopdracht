dafa296c95dbc11a59620b99602947b0906411ef
Ziggy Maes <ziggy.maes@student.howest.be>
2016-04-18 09:24:46 +0200

    Compabiliteit Robin

--- Project/build-tmp/source/Project.java
+++ Project/build-tmp/source/Project.java
@@ -1,13 +1,39 @@
-import g4p_controls.G4P;
-import g4p_controls.GAlign;
-import g4p_controls.GButton;
-import g4p_controls.GCScheme;
-import g4p_controls.GEvent;
-import g4p_controls.GLabel;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
 
-import org.gamecontrolplus.Configuration;
-import org.gamecontrolplus.ControlDevice;
-import org.gamecontrolplus.ControlIO;
+import g4p_controls.G4P; 
+import g4p_controls.GAlign; 
+import g4p_controls.GButton; 
+import g4p_controls.GCScheme; 
+import g4p_controls.GEvent; 
+import g4p_controls.GLabel; 
+import org.gamecontrolplus.Configuration; 
+import org.gamecontrolplus.ControlDevice; 
+import org.gamecontrolplus.ControlIO; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Project extends PApplet {
+
+
+
+
+
+
+
+
+
+
+
 
 ControlIO controlIO;
 int panelHeight;
@@ -22,7 +48,7 @@ int test = 100;
 boolean testingGamePad = false;
 
 public void setup() {
-  size(800,600);
+  
   controlIO = ControlIO.getInstance(this);
   fill(255);
   /*test = controlIO.getMatchedDevice("test");
@@ -100,7 +126,7 @@ public void draw() {
     ellipse(bolX,bolY,10,10);
   }
 }
-void keyPressed() {
+public void keyPressed() {
   if (key == CODED) {
       if (keyCode == UP) {
         bolY-=10;
@@ -127,4 +153,14 @@ public void schiet(int projectielX, int projectielY){
       ellipse(bolX,bolY,10,10);
       //background(0);
     }
+}
+  public void settings() {  size(800,600); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Project" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
